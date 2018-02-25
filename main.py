99b9fe1dd44758ba54b04498f174b4da331c1f95
James Ko <jamesqko@gmail.com>
2018-02-24 21:23:11 -0500

    Import JSON data

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import simplejson as json
+
+from pandas.io.json import json_normalize
+
 def main():
-    pass
+    with open('data/bullyingV3/tweet.json') as tweet_file:
+        tweet_data = json.load(tweet_file)
+    
+    for tweet in tweet_data:
+        if 'entities' in tweet:
+            del tweet['entities']
+        if 'user' in tweet:
+            del tweet['user']
+
+    df = json_normalize(tweet_data)
+    print(df.head(n=10))
+    print(df.columns.values)
 
 if __name__ == '__main__':
     main()
