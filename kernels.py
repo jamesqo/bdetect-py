a3dc3821a600eea121ad730c2a78582d67513220
James Ko <jamesqko@gmail.com>
2018-02-26 19:52:00 -0500

    No touching for now

--- kernels.py
+++ kernels.py
@@ -1,3 +1,10 @@
+class TweetKernel(object):
+    def __init__(self, tree_kernel):
+        self.tree_kernel = tree_kernel
+
+    def __call__(self, x, y):
+        print(type(x), type(y))
+
 class PTKernel(object):
     def __init__(self):
         pass
