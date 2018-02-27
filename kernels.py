42c9d445dda03023b23f60571d8291b6db8eb70e
James Ko <jamesqko@gmail.com>
2018-02-26 20:40:03 -0500

    Finally fix, I think

--- kernels.py
+++ kernels.py
@@ -4,8 +4,8 @@ class TweetKernel(object):
         self.tree_kernel = tree_kernel
 
     def __call__(self, x, y):
-        x = self.docs[x]
-        y = self.docs[y]
+        x_doc = self.docs[x['doc_index']]
+        y_doc = self.docs[y['doc_index']]
         return 0
 
 class PTKernel(object):
