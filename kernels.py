3ce20df985a74c3020a5c49422a5b98816fd9042
James Ko <jamesqko@gmail.com>
2018-02-26 20:45:46 -0500

    Finally fix for real

--- kernels.py
+++ kernels.py
@@ -1,12 +1,21 @@
+def _get_tree_kernel_function(name):
+    if name == 'ptk':
+        return PTKernel()
+
+    raise ValueError(f"Unrecognized tree kernel '{name}'")
+
+DOC_INDEX = 0
+
 class TweetKernel(object):
     def __init__(self, docs, tree_kernel):
         self.docs = docs
         self.tree_kernel = tree_kernel
+        self._tree_kernel_function = _get_tree_kernel_function(name=tree_kernel)
 
     def __call__(self, x, y):
-        x_doc = self.docs[x['doc_index']]
-        y_doc = self.docs[y['doc_index']]
-        return 0
+        x_doc = self.docs[x[DOC_INDEX]]
+        y_doc = self.docs[y[DOC_INDEX]]
+        return self._tree_kernel_function(x_doc, y_doc)
 
 class PTKernel(object):
     def __init__(self):
