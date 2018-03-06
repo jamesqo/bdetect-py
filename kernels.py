9ffd3adca629e1843664caf87c44dfc003930b3b
James Ko <jamesqko@gmail.com>
2018-03-05 21:27:23 -0500

    [WIP] Port code from http://joedsm.altervista.org/pythontreekernels.htm

--- kernels.py
+++ kernels.py
@@ -4,22 +4,63 @@ def _get_tree_kernel_function(name):
 
     raise ValueError(f"Unrecognized tree kernel '{name}'")
 
+
 DOC_INDEX = 0
+
 
 class TweetKernel(object):
     def __init__(self, docs, tree_kernel):
         self.docs = docs
         self.tree_kernel = tree_kernel
-        self._tree_kernel_function = _get_tree_kernel_function(name=tree_kernel)
+        self._tree_kernel_function = _get_tree_kernel_function(
+            name=tree_kernel)
 
     def __call__(self, x, y):
         xindex, yindex = int(x[DOC_INDEX]), int(y[DOC_INDEX])
         xdoc, ydoc = self.docs[xindex], self.docs[yindex]
         return self._tree_kernel_function(xdoc, ydoc)
 
-class PTKernel(object):
-    def __init__(self):
-        pass
 
-    def __call__(self, x, y):
+class PTKernel(object):
+    def __init__(self, lambda_, mu):
+        self.lambda_ = lambda_
+        self.mu = mu
+        self._lambda2 = lambda_ ** 2
+
+    def __call__(self, a, b):
         return 0
+
+    def delta(self, a, b):
+        if a.getOutdegree() == 0 or b.getOutdegree() == 0:
+            result = self.mu * self._lambda2
+        else:
+            nca, ncb = a.getOutdegree(), b.getOutdegree()
+            result = self.mu * (self._lambda2 + self.sigma_delta_p(a, b, nca, ncb))
+        return result
+
+    def sigma_delta_p(self, a, b, nca, ncb):
+        DPS = [[0] * (ncb + 1)] * (nca + 1)
+        DP = [[0] * (ncb + 1)] * (nca + 1)
+        kmat = [0] * (nca + 1)
+        for i in range(1, nca + 1):
+            for j in range(1, ncb + 1):
+                if a.getChild(i - 1).getLabel() == b.getChild(j - 1).getLabel():
+                    DPS[i][j] = self.delta(a.getChild(i - 1), b.getChild(j - 1))
+                    kmat[0] += DPS[i][j]
+                else:
+                    DPS[i][j] = 0
+
+        for s in range(1, min(nca, ncb)):
+            for i in range(nca + 1):
+                DP[i][s - 1] = 0
+            for j in range(ncb + 1):
+                DP[s - 1][j] = 0
+            for i in range(s, nca + 1):
+                for j in range(s, ncb + 1):
+                    DP[i][j] = DPS[i][j] + self.lambda_ * DP[i - 1][j] + \
+                               self.lambda_ * DP[i][j - 1] - self._lambda2 * DP[i - 1][j - 1]
+                    if a.getChild(i - 1).getLabel() == b.getChild(j - 1).getLabel():
+                        DPS[i][j] = self.delta(a.getChild(i - 1), b.getChild(j - 1)) * DP[i - 1][j - 1]
+                        kmat[s] += DPS[i][j]
+
+        return sum(kmat)
